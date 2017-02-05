608e1eb1a013a8598954115259768e5f2864d47b
AlphaHelixDev <max.hertel@outlook.de>
2017-02-05 19:56:54 +0100

    Init commit

---
+++ src/main/java/de/alphahelix/skygames/SkyGames.java
@@ -0,0 +1,12 @@
+package de.alphahelix.skygames;
+
+import de.alphahelix.alphalibary.fakeapi.FakeAPI;
+
+public class SkyGames extends FakeAPI {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+
+    }
+}
